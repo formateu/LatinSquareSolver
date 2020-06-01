c5c21995c5f2c1f539b41ca03e7a0596434d4acf
Mateusz Forc <formateu@gmail.com>
2020-06-01 21:53:22 +0200

    Add initial conditions apply

---
+++ latin.hs
@@ -0,0 +1,124 @@
+import Data.Char
+import Data.List
+
+data Cell = Fixed Int | Possible [Int] deriving (Show, Eq)
+type Row  = [Cell]
+type Grid = [Row]
+
+showGrid :: Grid -> String
+showGrid = unlines . map (unwords . map showCell)
+  where
+    showCell (Fixed x) = show x
+    showCell _ = "."
+
+showGridWithPossibilities :: Grid -> String
+showGridWithPossibilities = unlines . map (unwords . map showCell)
+  where
+    showCell (Fixed x)     = show x ++ "          "
+    showCell (Possible xs) =
+      (++ "]")
+      . Data.List.foldl' (\acc x -> acc ++ if x `elem` xs then show x else " ") "["
+      $ [1..4]
+
+generateCell :: Int -> Cell
+generateCell size = Possible [1..size]
+
+generateRow :: Int -> Row
+generateRow size = generateInternal size size
+    where generateInternal 0 _ = []
+          generateInternal currPos n = (generateCell size):(generateInternal (currPos-1) n)
+
+generateGrid :: Int -> Grid
+generateGrid size = generateInternal size size
+    where generateInternal 0 _ = []
+          generateInternal currPos n = (generateRow size):(generateInternal (currPos-1) n)
+
+readViews :: String -> [Int]
+readViews s = fmap readCell s
+    where readCell '.' = 0
+          readCell c = Data.Char.digitToInt $ c
+
+readInt :: IO Int
+readInt = do
+    int <- getLine
+    return (read int)
+
+removeFromList :: Eq a => a -> [a] -> [a]
+removeFromList _ []                 = []
+removeFromList x (y:ys) | x == y    = removeFromList x ys
+                    | otherwise = y : removeFromList x ys
+
+removeFrom :: Int -> Cell -> Cell
+removeFrom toRemove (Fixed value)
+    | value == toRemove = Possible []
+
+removeFrom toRemove (Possible intList) = Possible (removeFromList toRemove intList)
+
+removeIncremental :: Int -> Int -> Row -> Row
+removeIncremental howManyWeSee problemSize row = removeGEGrowing (problemSize-howManyWeSee+2) problemSize row
+
+removeGEGrowing :: Int -> Int -> Row -> Row
+removeGEGrowing h p (x:xs)
+    | h <= p =  (removeGE h x):(removeGEGrowing (h+1) p xs)
+    | otherwise = x:xs
+
+removeGEGrowing _ _ [] = []
+
+removeGE :: Int -> Cell -> Cell
+removeGE toRemove (Possible list) = Possible (removeGEInt toRemove list)
+
+removeGE toRemove (Fixed n)
+    | n == toRemove = Possible []
+    | otherwise = Fixed n
+
+removeGEInt :: Int -> [Int] -> [Int]
+removeGEInt toRemove (l:ls)
+    | l < toRemove = l:(removeGEInt toRemove ls)
+    | otherwise = []
+
+applyViewImpl :: Int -> Int -> Row -> Row
+applyViewImpl problemSize howManyWeSee (xs:ys)
+    | howManyWeSee == 0 = (xs:ys)
+    | howManyWeSee == problemSize = fmap Fixed [1..problemSize]
+    | howManyWeSee == 1 = (Fixed problemSize):ys
+    | howManyWeSee == 2 = (removeFrom problemSize xs):ys
+    | otherwise = removeIncremental howManyWeSee problemSize (xs:ys)
+
+applyView :: Int -> [Int] -> (Grid -> Grid) -> Grid -> Grid
+applyView problemSize upperView transformFunc grid = transformFunc (map (\(a,b) -> applyViewImpl problemSize a b) $ zip upperView $ transformFunc grid)
+
+applyUpperView :: Int -> [Int] -> Grid -> Grid
+applyUpperView problemSize upperView grid = applyView problemSize upperView transpose grid
+
+applyLeftView :: Int -> [Int] -> Grid -> Grid
+applyLeftView problemSize leftView grid =  applyView problemSize leftView id grid
+
+applyRightView :: Int -> [Int] -> Grid -> Grid
+applyRightView problemSize rightView grid = applyView problemSize rightView (transpose . reverse . transpose) grid
+
+applyBottomView :: Int -> [Int] -> Grid -> Grid
+applyBottomView problemSize bottomView grid = applyView problemSize bottomView (reverse . transpose) grid
+
+-- test
+grid = generateGrid 4
+applyLeft = applyLeftView 4 [0,0,4,0] grid
+applyRight = applyRightView 4 [0,3,0,0] grid
+applyUp = applyUpperView 4 [3,0,1,0] grid
+applyBottom = applyBottomView 4 [0, 0, 0, 0] grid
+
+-- main :: IO ()
+-- main = do print "Enter problem size"
+--           problemSize <- readInt
+--           let possibleGrid = generateGrid problemSize
+--           print "Enter upper views"
+--           upperViewsLine <- getLine
+--           let upperViews = readViews upperViewsLine
+--           print "Enter left views"
+--           leftViewsLine <- getLine
+--           let leftViews = readViews leftViewsLine
+--           print "Enter right views"
+--           rightViewsLine <- getLine
+--           let rightViews = readViews rightViewsLine
+--           print "Enter bottom views"
+--           bottomViewsLine <- getLine
+--           let bottomViews = readViews bottomViewsLine
